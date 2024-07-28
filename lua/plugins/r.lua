2b9ad5367d732bde214a91dcaf18e16b9366ae39
Fernando Andres-Pretel <feranpre@gmail.com>
2024-07-28 11:59:22 +0200

    assdf

--- lua/plugins/r.lua
+++ lua/plugins/r.lua
@@ -5,5 +5,29 @@ return {
 
     { "<C-CR>", mode = "n", "<Plug>RDSendLine", { desc = "send [l]ine and move down" } },
     { "<C-CR>", mode = "v", "<Plug>RDSendSelection", { desc = "send [l]ine and move down" } },
+    {
+      "<localleader>bl",
+      mode = "n",
+      "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
+      { desc = "[l]oad all files" },
+    },
+    {
+      "<localleader>bb",
+      mode = "n",
+      "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
+      { desc = "[b]uild pkg" },
+    },
+    {
+      "<localleader>bi",
+      mode = "n",
+      "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
+      { desc = "[i]nstall pkg" },
+    },
+    {
+      "<localleader>bd",
+      mode = "n",
+      "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
+      { desc = "[d]ocument pkg" },
+    },
   },
 }
