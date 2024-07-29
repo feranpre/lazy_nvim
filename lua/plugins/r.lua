d66d3f7afe1d6d13f2acfca364cf8ca4dcba526c
Fernando Andres-Pretel <feranpre@gmail.com>
2024-07-29 13:52:57 +0200

    asedf

--- lua/plugins/r.lua
+++ lua/plugins/r.lua
@@ -3,31 +3,31 @@ return {
   -- 'jalvesaq/Nvim-R', -- this plugin is obsolete
   keys = {
 
-    { "<C-CR>", mode = "n", "<Plug>RDSendLine", { desc = "send [l]ine and move down" } },
-    { "<C-CR>", mode = "v", "<Plug>RDSendSelection", { desc = "send [l]ine and move down" } },
+    { "<C-CR>", mode = "n", "<Plug>RDSendLine", desc = "send [l]ine and move down" },
+    { "<C-CR>", mode = "v", "<Plug>RDSendSelection", desc = "send [l]ine and move down" },
     {
       "<localleader>bl",
       mode = "n",
       "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
-      { desc = "[l]oad all files" },
+      desc = "[l]oad all files",
     },
     {
       "<localleader>bb",
       mode = "n",
-      "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
-      { desc = "[b]uild pkg" },
+      "<cmd>lua require('r.send').cmd('devtools::build()')<CR>",
+      desc = "[b]uild pkg",
     },
     {
       "<localleader>bi",
       mode = "n",
-      "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
-      { desc = "[i]nstall pkg" },
+      "<cmd>lua require('r.send').cmd('devtools::install(args = \"--preclean --with-keep.source --no-multiarch\")')<CR>",
+      desc = "[i]nstall pkg",
     },
     {
       "<localleader>bd",
       mode = "n",
-      "<cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
-      { desc = "[d]ocument pkg" },
+      '<cmd>lua require(\'r.send\').cmd(\'devtools::document(roclets = c("rd", "collate", "namespace", "vignette"))\')<CR>',
+      desc = "[d]ocument pkg",
     },
   },
 }
